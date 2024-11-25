0f1c539b041bba8a865dc1fd2fef5aeeece2eca1
Marcele Elizabeth Aiache de Moraes <marcele.moraes@grupoaguasdobrasil.com.br>
2024-11-25 13:57:31 -0300

    More functions in my code

--- vendas.py
+++ vendas.py
@@ -2,14 +2,13 @@ import sqlalchemy as sa
 
 engine = sa.create_engine('sqlite:///C:/temp/PANDA/PUC/BANCO_DE_DADOS_RELACIONAL/aula02/vendas.db')
 
-
 import sqlalchemy.orm as orm
 
 base = orm.declarative_base()
 
 #tb cliente 
 class cliente(base):
-    __tablename__ = "cliente"
+    __tablename__ = "tbCliente"
     
     cpf = sa.Column(sa.CHAR(14), nullable = False, primary_key = True, index = True)
     nome = sa.Column(sa.VARCHAR(100), nullable = False)
@@ -22,7 +21,7 @@ class cliente(base):
 
 #tb fornecedor
 class fornecedor(base):
-    __tablename__ = "fornecedor"
+    __tablename__ = "tbFornecedor"
     
     registro_fornecedor = sa.Column(sa.INTEGER, nullable = False, primary_key = True, index = True)
     nome_fantasia = sa.Column(sa.VARCHAR(100), nullable = False)
@@ -32,30 +31,31 @@ class fornecedor(base):
 
 #tb produto
 class produto(base):
-    __tablename__ = "produto"
+    __tablename__ = "tbProduto"
     
     codigo_barras = sa.Column(sa.INTEGER, nullable = False, primary_key = True, index = True)
-    registro_fornecedor = sa.Column(sa.INTEGER, sa.ForeignKey("fornecedor.registro_fornecedor", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
-    dc_produto = sa.Column(sa.VARCHAR(100), nullable = False)
+    registro_fornecedor = sa.Column(sa.INTEGER, sa.ForeignKey("tbFornecedor.registro_fornecedor", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
+    dsc_produto = sa.Column(sa.VARCHAR(100), nullable = False)
     genero = sa.Column(sa.CHAR(1))
 
 #tb vendedor
 class vendedor(base):
-    __tablename__ = "vendedor"
+    __tablename__ = "tbVendedor"
     
     registro_vendedor = sa.Column(sa.INTEGER, nullable = False, primary_key = True, index = True)
+    cpf = sa.Column(sa.VARCHAR(14), nullable = False)
     nome = sa.Column(sa.VARCHAR(100), nullable = False)
     genero = sa.Column(sa.CHAR(1), nullable = False)
     email = sa.Column(sa.VARCHAR(50), nullable = False)
 
 #tb venda
 class venda(base):
-    __tablename__ = "venda"
+    __tablename__ = "tbVenda"
     
     idTransacao = sa.Column(sa.INTEGER, nullable = False, primary_key = True, index = True)
-    cpf = sa.Column(sa.CHAR(14), sa.ForeignKey("cliente.cpf", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
-    codigo_barras = sa.Column(sa.INTEGER, sa.ForeignKey("produto.codigo_barras", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
-    registro_vendedor = sa.Column(sa.INTEGER, sa.ForeignKey("vendedor.registro_vendedor", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
+    cpf = sa.Column(sa.CHAR(14), sa.ForeignKey("tbCliente.cpf", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
+    codigo_barras = sa.Column(sa.INTEGER, sa.ForeignKey("tbProduto.codigo_barras", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
+    registro_vendedor = sa.Column(sa.INTEGER, sa.ForeignKey("tbVendedor.registro_vendedor", ondelete="NO ACTION", onupdate="CASCADE"), index = True)
     vlr_venda = sa.Column(sa.DECIMAL(10,2), nullable = False)
     data_hora_venda = sa.Column(sa.DATETIME(), nullable = False)
   
@@ -66,4 +66,3 @@ try:
 
 except ValueError:
     ValueError()
-
--- vendedor.py
+++ vendedor.py
@@ -6,28 +6,53 @@ import vendas as vd
 endereco = "C:\\temp\\PANDA\\PUC\\BANCO_DE_DADOS_RELACIONAL\\aula02\\"
 
 vendedor = pd.read_csv(endereco + "vendedor.csv", sep=";")
-vendedor = pd.DataFrame(vendedor)
+tbVendedor = pd.DataFrame(vendedor)
 
-engine = sa.create_engine('sqlite:///C:/temp/PANDA/PUC/BANCO_DE_DADOS_RELACIONAL/vendas.db')
+engine = sa.create_engine("sqlite:///C:/temp/PANDA/PUC/BANCO_DE_DADOS_RELACIONAL/aula02/vendas.db")
 
 sessao = orm.sessionmaker(bind=engine)
 sessao = sessao()
 
-for i in range(len(vendedor)):
+for i in range(len(tbVendedor)):
     dados_vendedor = vd.vendedor(
-        registro_vendedor = int(vendedor["registro_vendedor"][i]),
+        registro_vendedor = int(tbVendedor["registro_vendedor"][i]),
         cpf = vendedor["cpf"][i],
-        nome = vendedor["nome"],
+        nome = vendedor["nome"][i],
         genero = vendedor["genero"][i],
         email = vendedor["email"][i]
     )
     try:
         sessao.add(dados_vendedor)
-        sessao.commit
+        sessao.commit()
+        
     except ValueError:
         ValueError()
-        
-        
+        sessao.rollback()
+       
 print("Dados inseridos na tabela vendedor")
 
+produto = pd.read_excel(endereco + "produto.xlsx")
 
+tbProduto = pd.DataFrame(produto)
+
+conn = engine.connect()
+
+# metodo descontinuado -> metadata = sa.schema.MetaData(bind=engine)
+
+metadata = sa.schema.MetaData()
+metadata.bind = engine
+
+DadosProduto = tbProduto.to_dict(orient="records")
+
+tabela_produto = sa.Table(vd.produto.__tablename__, metadata, autoload_with=engine) # descontinuado -> autoload=True
+
+try:
+    conn.execute(tabela_produto.insert(), DadosProduto)
+    sessao.commit()
+except ValueError:
+    ValueError()
+    
+print("Dados inseridos na tabela tbProduto")
+
+sessao.close_all()
+    
